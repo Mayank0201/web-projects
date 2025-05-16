87de15a2b8156875ae03652cfeddfd9e5a2d3113
Mayank0201 <136915420+Mayank0201@users.noreply.github.com>
2025-05-16 12:27:58 +0530

    Update index.js

--- Blogging web app/index.js
+++ Blogging web app/index.js
@@ -14,6 +14,10 @@ app.get("/", (req, res) => {
     res.render("index.ejs")
 });
 
+app.get("/home",(req,res) => {
+    res.render("home.ejs");
+})
+
 app.get("/create",(req,res) => {
     res.render("create.ejs")
 })
